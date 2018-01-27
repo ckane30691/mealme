de8c0934520a636ab68e0f26d0791f3f663e41cf
Cory Kane <ckane30691@gmail.com>
2018-01-27 13:51:12 -0800

    Set up file structure for frontend

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ plugins = plugins.concat(
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/basechamp_entry.jsx",
+  entry: "./frontend/entry.jsx",
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
