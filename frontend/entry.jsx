b6aa14ca297e7ff35b6bd1d3a03791f5b452a198
Cory Kane <ckane30691@gmail.com>
2018-01-27 23:47:33 -0800

    Set up store to interact with API endpoint and connect googlemaps api with lat and long info from api endpoint

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,35 +1,48 @@
 import configureStore from './store/store';
 import {sendLunchTimeLocation} from './actions/hunger_actions';
+import { fetchEaters } from './util/eater_api_util';
+
+
 
 document.addEventListener("DOMContentLoaded", function() {
-  const store = configureStore();
-  console.log(store.getState());
-  var loadMapScript = document.createElement('script');
-  loadMapScript.onload = function() {
-    alert("about to load the map");
-  };
+  const preload = fetchEaters().then(res => {
+    const store = configureStore({ entities: { hunger: res } });
+    var loadMapScript = document.createElement('script');
+    loadMapScript.onload = function() {
+      // alert("about to load the map");
+    };
+    const hungryEaters = store.getState().entities.hunger;
+    window.initMap = function initMap() {
+      var sfBay = {lat: 37.770691, lng: -122.407650};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: sfBay
+      });
+      Object.values(hungryEaters).forEach(eater => {
+        let uluru = { lat: parseFloat(eater.loc_x), lng: parseFloat(eater.loc_y) };
+        console.log(hungryEaters);
+        new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      });
+      // var marker = new google.maps.Marker({
+      //   position: uluru,
+      //   map: map
+      // });
+    };
 
-  window.initMap = function initMap() {
-    var uluru = {lat: 37.770691, lng: -122.407650};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: uluru
+    const apiKey = require("./envVariables.js");
+    loadMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+    document.getElementsByTagName("head")[0].appendChild(loadMapScript);
+
+    document.getElementById("submit_lunch_location")
+    .addEventListener("click", function() {
+      let data = document.getElementById("lunch_location_input").value;
+      sendLunchTimeLocation(data);
+      // console.log("worked");
     });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-  };
 
-  const apiKey = require("./envVariables.js");
-  loadMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
-  document.getElementsByTagName("head")[0].appendChild(loadMapScript);
-
-  document.getElementById("submit_lunch_location")
-          .addEventListener("click", function() {
-            let data = document.getElementById("lunch_location_input").value;
-            sendLunchTimeLocation(data);
-            // console.log("worked");
-          });
+  });
 
 });
