14f724db77cdb8e8a8f6984fd4a1b4e513fdbf2f
Cory Kane <ckane30691@gmail.com>
2018-01-27 17:42:12 -0800

    Remove React and implement vanilla js implementation for gmaps api

---
+++ frontend/entry.jsx
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  var loadMapScript = document.createElement('script');
+  loadMapScript.onload = function() {
+    alert("about to load the map");
+  };
+
+  window.initMap = function initMap() {
+    var uluru = {lat: 37.770691, lng: -122.407650};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  };
+
+  const apiKey = require("./envVariables.js");
+  loadMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+  document.getElementsByTagName("head")[0].appendChild(loadMapScript);
+});
